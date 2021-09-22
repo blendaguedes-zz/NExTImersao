daedda1446d1d2fd2ef2d8e90072d99252d9bb36
Blenda Guedes <meu@email.com>
2021-09-22 18:45:43 -0300

    Adicionando uma interface.

---
+++ Animal/src/Aliementacao.java
@@ -0,0 +1,13 @@
+public interface Aliementacao {
+
+
+    public String regime();
+
+    // Exemplo de ilegal
+    // public final String comportamento();
+
+    public abstract String comportamento();
+
+
+    
+}
