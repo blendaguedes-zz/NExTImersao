dbc6baf21b01f0fb138fb32aae3f2b0a691aab99
Blenda Guedes <meu@email.com>
2021-09-21 22:02:44 -0300

    Alteracao no metodo Main do App

--- Animal/src/App.java
+++ Animal/src/App.java
@@ -1,6 +1,41 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
-        
+
+        System.out.println("Escreva qual tipo de animal você quer criar:\nCachorro\nPeixe\nCobra");
+        Scanner sc = new Scanner(System.in);
+
+        String tipoAnimal = sc.nextLine();
+
+        switch(tipoAnimal){
+            case "Cachorro":
+                Cachorro cachorro = new Cachorro();
+                caractetisticasAnimal(tipoAnimal, cachorro);
+                break;
+
+            case "Cobra":
+                Cobra cobra = new Cobra();
+                caractetisticasAnimal(tipoAnimal, cobra);
+                break;
+            
+            case "Peixe":
+                Peixe peixe = new Peixe();
+                caractetisticasAnimal(tipoAnimal, peixe);
+                break;
+            default:
+                System.out.println("Animal inválido!");           
+
+        }
+
+        sc.close();
+    }
+
+    public static void caractetisticasAnimal(String tipoAnimal, Animal animal){
+
+        System.out.println("O " + tipoAnimal + " é " + animal.alimentar() +
+        ".\nEste animal se locomove " + animal.locomover() + ". E emite o som ");
+        animal.emitirSom();
     }
 }
